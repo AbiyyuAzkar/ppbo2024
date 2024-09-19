059c0b7e5dc407908569f35c0329a376e1eaaf6f
AbiyyuAzkar <h1101231050@student.untan.ac.id>
2024-09-19 21:56:21 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Admin;
+class Pegawai{
+    public $nip;
+    public $nama;
+    protected $no_hp;
+    public $alamat;
+
+    public function __construct($nip, $nama, $no_hp, $alamat){
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+    public function cekIn(): bool {
+        echo $this->nama . " berhasil cek in.<br>";
+        return true;
+    }
+
+    public function cekOut(): bool {
+        echo $this->nama . " berhasil cek out.<br>";
+        return true;
+    }
+
+    public function getNoHp(): int {
+        return $this->no_hp;
+    }
+
+    public function setNoHp($no_hp): void {
+        $this->no_hp = $no_hp;
+    }
+}
+class Dosen extends Pegawai {
+    public string $nidn;
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, string $nidn) {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->nidn = $nidn;
+    }
+    public function mengajar(): void {
+        echo $this->nama . " sedang mengajar perkuliahan";
+    }
+}
+class TenagaKependidikan extends Pegawai {
+    public int $gaji_pokok;
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, int $gaji_pokok) {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->gaji_pokok = $gaji_pokok;
+    }
+    public function cuti(): void {
+        echo $this->nama . " sedang cuti";
+    }
+}
