dc19cca1f6e07377a724b7a6c4efe7188ccd5600
AbiyyuAzkar <h1101231050@student.untan.ac.id>
2024-09-20 14:16:24 +0700

    Praktikum 6A - Menyusun Ulang Namespace dan Struktur Direktori

---
+++ App/Model/Pustaka/Author.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Model\Pustaka;
+class Author
+{
+    public $name;
+    public $description;
+
+    public function __construct($name, $description) {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function show() {
+        echo "Author: {$this->name}\n";
+        echo "Description: {$this->description}\n";
+    }
+}
---
+++ App/Model/Pustaka/Publisher.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Model\Pustaka;
+class Publisher 
+{
+    public $name;
+    public $address;
+    public $phone;
+
+    public function __construct($name, $address, $phone) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->phone = $phone;
+    }
+
+    public function setPhone($phone) {
+        $this->phone = $phone;
+    }
+
+    public function getPhone() {
+        return $this->phone;
+    }
+
+    public function show() {
+        echo "Publisher: {$this->name}\n";
+        echo "Address: {$this->address}\n";
+        echo "Phone: {$this->phone}\n";
+    }
+}
